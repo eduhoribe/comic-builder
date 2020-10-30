43e8fadc2bb8e83a0e61c11e2ac85c16404c6484
Eduwardo Horibe <eduhoribe@gmail.com>
2020-10-30 00:58:08 -0300

    Big metadata improvements

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import urllib.request
+import json
 import os
 import random
 import re
@@ -16,6 +18,16 @@ if __name__ == '__main__':
     random_id = hex(hash(random.random()))
 
     # TODO: Parameterize
+    directory = 'Manga/Youjo Senki'
+
+    manga_info_file = join(directory, 'info.json')
+
+    manga_info = {}
+    if os.path.isfile(manga_info_file):
+        with open(manga_info_file, 'r') as manga_info:
+            manga_info = json.load(manga_info)
+
+    # TODO: Parameterize
     device_profile = 'KO'  # TODO ENUM
     comic_style = ComicStyle.NORMAL
     low_quality = False
@@ -26,7 +38,6 @@ if __name__ == '__main__':
     stretch = True
     grayscale = False
     auxiliary_folder = '/home/eduhoribe'
-    directory = '/home/eduhoribe/Manga/Youjo Senki'
 
     temp_directory = '{}/manga-py-assembler/{}_{}'.format(
         auxiliary_folder if auxiliary_folder is not None else tempfile.gettempdir(), random_id, comic_title)
@@ -37,6 +48,9 @@ if __name__ == '__main__':
 
     chapters = []
     volumes = {}
+
+    publishers = set()
+    languages = set()
 
     # Read files
     files = [file for file in listdir(directory) if isfile(join(directory, file)) and file.endswith('.zip')]
@@ -51,6 +65,9 @@ if __name__ == '__main__':
 
                     chapters.append(chapter)
 
+                    publishers.add(chapter.publisher)
+                    languages.add(chapter.language)
+
     # Merge volumes
     for chapter in chapters:
         if chapter.volume not in volumes:
@@ -60,13 +77,13 @@ if __name__ == '__main__':
         volumes_chapters.append(chapter)
 
     for volume, chapters in volumes.items():
-        volume_directory = join(temp_directory, 'volume_{}'.format(volume))
+        volume_directory = join(temp_directory, '{} - Volume {}'.format(comic_title, volume))
         os.mkdir(volume_directory)
 
         print(volume)
 
         for chapter in chapters:
-            chapter_directory = join(volume_directory, 'chapter_{}'.format(chapter.chapter))
+            chapter_directory = join(volume_directory, 'Chapter {}: {}'.format(chapter.chapter, chapter.title))
             os.mkdir(chapter_directory)
 
             with zipfile.ZipFile(chapter.local_path, "r") as zip_file:
@@ -79,10 +96,10 @@ if __name__ == '__main__':
         print()
         break
 
-    assembled_ebooks = []
+    assembled_ebooks = {}
 
     for volume in volumes.keys():
-        volume_temp_data = join(temp_directory, 'volume_{}'.format(volume))
+        volume_temp_data = join(temp_directory, '{} - Volume {}'.format(comic_title, volume))
         volume_output = output if output is not None else join(directory, 'assembled')
         if not os.path.isdir(volume_output):
             os.makedirs(volume_output)
@@ -103,28 +120,38 @@ if __name__ == '__main__':
 
         success = kcc_c2e(args) == 0  # success exit code
 
-        if success:
-            shutil.rmtree(volume_temp_data)
-            assembled_ebooks.append(join(volume_output, 'volume_{}.{}'.format(volume, comic_format.lower())))
+        # if success:
+        #     shutil.rmtree(volume_temp_data)
+        assembled_ebooks[volume] = (
+            join(volume_output, '{} - Volume {}.{}'.format(comic_title, volume, comic_format.lower())))
 
-            break
+        break
 
     # Calibre metadata
     if os.system('ebook-meta --version') == 0:
-        for ebook_file in assembled_ebooks:
+        for volume, ebook_file in assembled_ebooks.items():
+            cloud_cover_url = manga_info['covers'][int(volume) - 1]
+            response = urllib.request.urlopen(cloud_cover_url)
+
+            local_cover_path = join(temp_directory, '{} - Volume {}'.format(comic_title, volume), 'cover')
+            with open(local_cover_path, 'wb') as local_cover:
+                local_cover.write(response.read())
+
+            authors = {manga_info['author'], manga_info['artist']}
+
             args = [
-                '--title="{}"'.format(comic_title),
-                '--authors="{}"'.format('YARE YARE DAZE'),
-                # '--cover="{}"',
-                # '--comments="{}"',
-                # '--publisher="{}"',
-                # '--category="{}"',
-                # '--series="{}"',
-                # '--index="{}"',
-                # '--identifier="{}"',
+                '--title="{} - Volume {}"'.format(manga_info['title'], volume),
+                '--authors="{}"'.format('&'.join(a for a in authors)),
+                '--cover="{}"'.format(local_cover_path),
+                '--comments="{}"'.format(manga_info['description']),
+                '--publisher="{}"'.format(', '.join([pub for pub in publishers])),
+                '--series="{}"'.format(manga_info['title']),
+                '--index="{}"'.format(volume),
+                '--identifier uri:"{}"'.format(manga_info['sauce']),
                 # '--tags="{}"',
-                # '--language="{}"',
-                # '--date="{}"',
+                '--book-producer="{}"'.format(', '.join([pub for pub in publishers])),
+                '--language="{}"'.format(', '.join([lang for lang in languages])),
+                '--date=""',
                 '"{}"'.format(ebook_file)
             ]
             args = [arg for arg in args if arg != '' and arg != '""']
